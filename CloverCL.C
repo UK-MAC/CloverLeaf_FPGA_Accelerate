d47e536e1d539fc6ebc2e8d0f05525f4c51ef058
A.Mallinson <acm@dcs.warwick.ac.uk>
2015-05-23 09:55:40 +0100

    started updating CloverCL.C for double2 new kernel

--- CloverCL.C
+++ CloverCL.C
@@ -1951,7 +1951,7 @@ void CloverCL::loadProgram(int xmin, int xmax, int ymin, int ymax)
     //build_one_program(xmin, xmax, ymin, ymax, "calc_dt_knl.aocx", &calc_dt_prog);
     //build_one_program(xmin, xmax, ymin, ymax, "min_reduction_knl.aocx", &min_reduction_prog);
 
-    build_one_program(xmin, xmax, ymin, ymax, "accelerate_removeMemOp_double4_960_3842_961_962.aocx", &accelerate_prog);
+    build_one_program(xmin, xmax, ymin, ymax, "accelerate_removeMemOp_double2_1921_3842_1922_1923.aocx", &accelerate_prog);
 
 
     //build_one_program(xmin, xmax, ymin, ymax, "accelerate_revert_knl.aocx", &calcdt_minred_prog);
